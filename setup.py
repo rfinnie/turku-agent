9ea83706416419b3cc2d8682988269adaf8f9c6d
Ryan Finnie <ryan@finnie.org>
2023-05-20 10:33:15 -0700

    Convert setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -5,29 +5,7 @@
 # SPDX-FileCopyrightText: Copyright (C) 2015-2021 Ryan Finnie <ryan@finnie.org>
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-import os
-from setuptools import setup
+import setuptools
 
-
-def read(filename):
-    with open(os.path.join(os.path.dirname(__file__), filename), encoding="utf-8") as f:
-        return f.read()
-
-
-setup(
-    name="turku_agent",
-    description="Turku backups - client agent",
-    long_description=read("README.md"),
-    long_description_content_type="text/markdown",
-    author="Ryan Finnie",
-    url="https://github.com/rfinnie/turku-agent",
-    python_requires="~=3.4",
-    packages=["turku_agent"],
-    install_requires=["requests"],
-    entry_points={
-        "console_scripts": [
-            "turku-agent-ping = turku_agent.ping:main",
-            "turku-update-config = turku_agent.update_config:main",
-        ]
-    },
-)
+if __name__ == "__main__":
+    setuptools.setup()
