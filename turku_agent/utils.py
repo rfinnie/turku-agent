8b22160aa24cbad64af465ba96d05355f9636c1d
Ryan Finnie <ryan@finnie.org>
2026-02-17 12:41:00 -0800

    SPDX header cleanup

--- turku_agent/utils.py
+++ turku_agent/utils.py
@@ -1,6 +1,8 @@
-# SPDX-PackageSummary: Turku backups - client agent
-# SPDX-FileCopyrightText: Copyright (C) 2015-2020 Canonical Ltd.
-# SPDX-FileCopyrightText: Copyright (C) 2015-2021 Ryan Finnie <ryan@finnie.org>
+# SPDX-PackageName: turku-agent
+# SPDX-PackageSupplier: Ryan Finnie <ryan@finnie.org>
+# SPDX-PackageDownloadLocation: https://github.com/rfinnie/turku-agent
+# SPDX-FileCopyrightText: © 2015 Canonical Ltd.
+# SPDX-FileCopyrightText: © 2015 Ryan Finnie <ryan@finnie.org>
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import copy
